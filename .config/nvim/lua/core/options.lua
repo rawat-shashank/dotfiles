f256fceaec15ff8d8ffe58ae61d84a1f620d9870
rawat-shashank <shashank17nov@gmail.com>
2025-04-29 07:19:50 +0100

    qol - nvim options update

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -18,6 +18,7 @@ opt.wrap = false
 -- search settings
 opt.ignorecase = true
 opt.smartcase = true
+opt.hlsearch = false
 
 -- cursor line
 opt.cursorline = true
@@ -26,6 +27,8 @@ opt.cursorline = true
 opt.termguicolors = true
 opt.background = "dark"
 opt.signcolumn = "yes"
+opt.scrolloff = 8
+opt.colorcolumn = "80,120"
 
 -- backspace
 opt.backspace = "indent,eol,start"
@@ -38,9 +41,11 @@ vim.opt.clipboard = "unnamedplus"
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", {
+		clear = true,
+	}),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
