5ec28772b1b8b77eca8c9b5cb64e9df1daa0fa56
rawat-shashank <shashank17nov@gmail.com>
2025-09-16 09:37:33 +0100

    added obsidian.nvim

--- config/nvim/lua/plugins/obsidian.lua
+++ config/nvim/lua/plugins/obsidian.lua
@@ -1,7 +1,13 @@
 return {
 	"obsidian-nvim/obsidian.nvim",
 	version = "*",
-	ft = "markdown",
+	cond = function()
+		-- Replace with the absolute path to your vault
+		return vim.fn.getcwd() == vim.fn.expand("~/obsidian/docs")
+	end,
+	keys = {
+		{ "<leader>dn", "<cmd>Obsidian today<cr>", noremap = true, silent = true, desc = "[D]aily [N]otes" },
+	},
 	---@module 'obsidian'
 	---@type obsidian.config
 	opts = {
@@ -30,6 +36,27 @@ return {
 			hl_group = "Comment",
 			separator = string.rep("-", 80),
 		},
+		-- remove legacy_commands support
 		legacy_commands = false,
+
+		-- Optional, customize how note IDs are generated given an optional title.
+		---@param title string|?
+		---@return string
+		note_id_func = function(title)
+			-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+			-- In this case a note with the title 'My new note' will be given an ID that looks
+			-- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+			local suffix = ""
+			if title ~= nil then
+				-- If title is given, transform it into valid file name.
+				suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+			else
+				-- If title is nil, just add 4 random uppercase letters to the suffix.
+				for _ = 1, 4 do
+					suffix = suffix .. string.char(math.random(65, 90))
+				end
+			end
+			return tostring(os.time()) .. "-" .. suffix
+		end,
 	},
 }
